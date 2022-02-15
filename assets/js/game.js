f68133c94619293cbcb6d5c9890814f927cc3475
Lemon Sama <burrisc24@gmail.com>
2022-02-15 14:01:09 -0500

    added js to allow us to fight more than one round and more than one robot

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,29 @@
-// var playerName = 'Clank McKrank';
+//Games States
+// "Win" - Player robot has defeated all enemy-robots
+//    *fight all enemy-robots
+//    Defeat each enemy-robot
+// "Lose" - Plsyer robot's health is zero or less
+
+
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+}
+
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -67,5 +78,6 @@ var fight = function() {
   }
 };
 
-// run fight function to start game
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
